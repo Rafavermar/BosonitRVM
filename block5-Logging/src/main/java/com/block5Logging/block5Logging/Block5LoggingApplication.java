5d74a6b5d35ac0380d864456be639e05aec6a5ba
RafaelVera <rafael.vera@bosonit.com>
2023-07-17 12:38:54 +0200

    block 5 ex Logging DONE

--- block5-Logging/src/main/java/com/block5Logging/block5Logging/Block5LoggingApplication.java
+++ block5-Logging/src/main/java/com/block5Logging/block5Logging/Block5LoggingApplication.java
@@ -1,13 +1,42 @@
 package com.block5Logging.block5Logging;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@SpringBootApplication
 public class Block5LoggingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Block5LoggingApplication.class, args);
-	}
+	private static final Logger logger = Logger.getLogger(Block5LoggingApplication.class.getName());
 
+	public static void main(String[] args) {
+		try {
+			// Configurar el manejador de archivo para el log de tipo "Error"
+			FileHandler fileHandler = new FileHandler("error.log");
+			fileHandler.setLevel(Level.SEVERE);
+			logger.addHandler(fileHandler);
+
+			// Ejemplo de logs
+			logger.severe("Este es un mensaje de Error");
+			logger.warning("Este es un mensaje de Warning");
+			logger.info("Este es un mensaje de Info");
+			logger.fine("Este es un mensaje de Fine");
+			logger.finer("Este es un mensaje de Finer");
+			logger.finest("Este es un mensaje de Finest");
+
+			// Configurar el nivel de log para la consola
+			logger.setLevel(Level.WARNING);
+
+			// Ejemplo de logs en la consola
+			logger.severe("Este es un mensaje de Error");
+			logger.warning("Este es un mensaje de Warning");
+			logger.info("Este es un mensaje de Info");
+			logger.fine("Este es un mensaje de Fine");
+			logger.finer("Este es un mensaje de Finer");
+			logger.finest("Este es un mensaje de Finest");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
