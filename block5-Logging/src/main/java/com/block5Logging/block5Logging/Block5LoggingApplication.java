b7f6eb49f14ab72d3d2e752c2231a96f4483f5dc
RafaelVera <rafael.vera@bosonit.com>
2023-07-17 11:09:05 +0200

    block5-Logging started

---
+++ block5-Logging/src/main/java/com/block5Logging/block5Logging/Block5LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.block5Logging.block5Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5LoggingApplication.class, args);
+	}
+
+}
---
+++ block5-Logging/src/test/java/com/block5Logging/block5Logging/Block5LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.block5Logging.block5Logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block5LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
