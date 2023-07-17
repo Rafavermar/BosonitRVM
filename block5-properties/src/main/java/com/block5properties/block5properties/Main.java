e76b1a7319b9c42d3f85b70edfbaf5f7f5636810
RafaelVera <rafael.vera@bosonit.com>
2023-07-17 10:37:50 +0200

    block5 ex2 (yml) DONE

--- block5-properties/src/main/java/com/block5properties/block5properties/Main.java
+++ block5-properties/src/main/java/com/block5properties/block5properties/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 public class Main {
 
 	@Value("${greeting}")
