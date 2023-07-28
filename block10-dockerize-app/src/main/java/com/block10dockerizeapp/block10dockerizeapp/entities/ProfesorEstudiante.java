0fada3ad5cb6235e1a5d274ce7b16e78b3268ff6
RafaelVera <rafael.vera@bosonit.com>
2023-07-28 12:28:22 +0200

    Block 10 Docker OK

--- block10-dockerize-app/src/main/java/com/block10dockerizeapp/block10dockerizeapp/entities/ProfesorEstudiante.java
+++ block10-dockerize-app/src/main/java/com/block10dockerizeapp/block10dockerizeapp/entities/ProfesorEstudiante.java
@@ -3,7 +3,6 @@ package com.block10dockerizeapp.block10dockerizeapp.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
--- block10-dockerize-app/src/main/java/com/block10dockerizeapp/block10dockerizeapp/entities/StudentEntity.java
+++ block10-dockerize-app/src/main/java/com/block10dockerizeapp/block10dockerizeapp/entities/StudentEntity.java
@@ -3,7 +3,6 @@ package com.block10dockerizeapp.block10dockerizeapp.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import java.util.ArrayList;
 import java.util.List;
