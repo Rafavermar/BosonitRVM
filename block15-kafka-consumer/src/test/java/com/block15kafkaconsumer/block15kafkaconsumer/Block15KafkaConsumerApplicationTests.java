19d8adc8b61630a323ebe7882970a40084cec6b6
RafaelVera <rafael.vera@bosonit.com>
2023-08-11 13:25:54 +0200

    block 15 Kafka OK

---
+++ block15-kafka-consumer/src/test/java/com/block15kafkaconsumer/block15kafkaconsumer/Block15KafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.block15kafkaconsumer.block15kafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block15KafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
