f9b1b17e4991874db6a3f5216fafb86e0d8b86d4
RafaelVera <rafael.vera@bosonit.com>
2023-08-14 11:57:10 +0200

    Block16- Eureka-Naming-Server OK

---
+++ block16-Eureka-Naming-Server/src/test/java/com/block16EurekaNamingServer/block16EurekaNamingServer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.block16EurekaNamingServer.block16EurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
